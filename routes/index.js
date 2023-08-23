f9a857874e35483e099ba00fc82bca0c0f12e893
Portablefire22 <barnowlsarecool@gmail.com>
2023-08-23 17:59:54 +0100

    Starting project functionality

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var projectInformation = require('./scripts/projectInformation.js');
+var projectInformation = require('../scripts/projectInformation.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) { 

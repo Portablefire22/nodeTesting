28a49827ec10bb11b468fd12bb36c23614428324
Portablefire22 <barnowlsarecool@gmail.com>
2023-08-24 00:22:52 +0100

    Project sites now fill the page with values from the project JSON File

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
+const fs = require('fs');
 var router = express.Router();
 var projectInformation = require('./../scripts/projectInformation.js');
-
+var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) { 
     res.render('pages/index', { title: 'Express' });
@@ -18,6 +19,16 @@ router.get('/projects', function(req, res, next){
 });
 
 router.get('/projects/Site', function(req, res, next){
-    res.render('pages/projects/thisSite', {});
+    res.render('pages/projects/thisSite', {projectInformation: getJson('../public/projects/thissite/thissite.json')});
 });
+
+router.get('/projects/computercraft', function(req, res, next){
+    res.render('pages/projects/computer', {projectInformation: getJson('../public/projects/computercraft/computercraft.json')});
+});
+
+function getJson(jsonPath) {
+    var pathProj = path.join(__dirname, jsonPath) 
+    return JSON.parse(fs.readFileSync(pathProj, 'utf-8'));
+}
+
 module.exports = router;

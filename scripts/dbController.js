055f93fdfc576ab91f3b87d842c3c7ef5c260b83
Portablefire22 <barnowlsarecool@gmail.com>
2023-08-28 00:35:32 +0100

    Changed table name because i was losing my mind

--- scripts/dbController.js
+++ scripts/dbController.js
@@ -12,23 +12,23 @@ const pool = mysql.createPool({
 let db = {};
 
 db.insertUser = async function(username, email, password) {
-    return await pool.query('INSERT INTO User (username, email, password) VALUES(?, ?, ?)', [username, email, password]);
+    return await pool.query('INSERT INTO Users (username, email, password) VALUES(?, ?, ?)', [username, email, password]);
 }
 
 db.getUserById = async function(id) {
-    return await pool.query('SELECT * FROM User WHERE id= ?', [id]);
+    return await pool.query('SELECT * FROM Users WHERE id= ?', [id]);
 }
 
 db.getUserByEmail = async function(email){
-    return await pool.query('SELECT * FROM User WHERE email = ?', [email]);
+    return await pool.query('SELECT * FROM Users WHERE email = ?', [email]);
 }
 
 db.getIdFromUsername = async function(username){
-    return await pool.query('SELECT id FROM User WHERE username = ?', [username]);
+    return await pool.query('SELECT id FROM Users WHERE username = ?', [username]);
 }
 
 db.getUserByName = async function(username){
-    return await pool.query('SELECT * FROM User WHERE username = ?', [username]);
+    return await pool.query('SELECT * FROM Users WHERE username = ?', [username]);
 }
 
 module.exports = db;
